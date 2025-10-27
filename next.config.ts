7b76dc55d42191dbdcd73dc7424497922a926fdb
cjy <13632362832@163.com>
2025-10-27 09:31:02 +0800

    second commit

---
+++ next.config.ts
@@ -0,0 +1,41 @@
+import type { NextConfig } from 'next';
+import os from 'os';
+import path from 'path';
+
+function getLocalIPList(): string[] {
+	const nets = os.networkInterfaces();
+	const results: string[] = [];
+
+	for (const name of Object.keys(nets)) {
+		for (const net of nets[name] || []) {
+			if (net.family === 'IPv4' && !net.internal) {
+				results.push(net.address);
+			}
+		}
+	}
+	return results;
+}
+
+const ip = getLocalIPList();
+
+const nextConfig: NextConfig = {
+	async redirects() {
+		return [
+			{
+				source: '/',
+				destination: '/filesGround',
+				permanent: true, // 或 false（临时重定向）
+			},
+		];
+	},
+	// 资源别名设置，需要配合tsconfig.json修改
+	turbopack: {
+		resolveAlias: {
+			'@': path.resolve(process.cwd(), 'src'),
+		},
+	},
+	// 跨域访问设置
+	allowedDevOrigins: [...ip],
+};
+
+export default nextConfig;
---
+++ src/app/(dashboard)/filesGround/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+	return (
+		<div className="">
+			<h1>文件蛆</h1>
+			<div className="p-4 bg-gray-100 text-blue-500 sm:text-lg">Hello</div>
+
+			<div className="text-base sm:text-lg md:text-xl lg:text-2xl">响应式字体</div>
+		</div>
+	);
+}
