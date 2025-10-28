fc92ba4d791ab0a2dee0d8bd5c6ba7a342ea0c3d
caiye <caiye2531889700@gmail.com>
2025-10-28 22:46:08 +0800

    home update

--- src/app/(dashboard)/filesGround/page.tsx
+++ src/app/(dashboard)/filesGround/page.tsx
@@ -1,7 +1,30 @@
 export default function Page() {
 	return (
 		<div className="">
-			<h1>文件蛆</h1>
+			<nav>
+				<ul>
+					<li>
+						<a href="#">菜单项 1</a>
+					</li>
+					<li>
+						<a href="#">菜单项 2</a>
+					</li>
+					<li>
+						<a href="#">菜单项 3</a>
+					</li>
+				</ul>
+			</nav>
+
+			{/* 功能蛆 */}
+			<ul className="">
+				<li>
+					<img src="" alt="" />
+					<p>excel 转 pdf</p>
+				</li>
+				<li>功能蛆 2</li>
+				<li>功能蛆 3</li>
+			</ul>
+
 			<div className="p-4 bg-gray-100 text-blue-500 sm:text-lg">Hello</div>
 
 			<div className="text-base sm:text-lg md:text-xl lg:text-2xl">响应式字体</div>
